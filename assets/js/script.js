1c42c25ddb24450480464441191dd66b50180a10
mytien <thaitiendrm@gmail.com>
2022-02-28 15:55:58 +0700

    up responsive code, not finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
-window.onscroll = () => window.scrollY > 200 ? scrollToTop.style.display = "block" : scrollToTop.style.display = "none"
-
-// Get the header
-var header = document.getElementById("myHeader");
 var scrollToTop = document.querySelector(".scrollToTop");
-// Get the offset position of the navbar
+var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
+
+window.onscroll = function () { myFunction() };
+scrollToTop.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+window.onscroll = () => window.scrollY > 200 ? scrollToTop.style.display = "block" : scrollToTop.style.display = "none"
+// Get the header
+// Get the offset position of the navbar
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset > sticky) {
@@ -16,4 +17,3 @@ function myFunction() {
     }
 }
 
-scrollToTop.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
