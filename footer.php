179321b0bc1c9f2406e46ff44e5c5a3fa0799231
TranThiHoaiTrang <68778348+TranThiHoaiTrang@users.noreply.github.com>
2022-03-01 10:48:15 +0700

    up

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer class="footer-contacts">
+        <div class="scrollToTop" style="display: none;">
+            <img src="assets/images/top.png" alt="Go Top">
+        </div>
+        <!-- CHUA CO ICON ZALO & HOTLINE -->
+       
+        <div class="top-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sx-1">
+                        <div class="fp-wrapper">
+                            <div class="img-fanpage">
+                                <img src="./assets/images/icon-facebook.png" alt="">
+                                <p>Fanpage Mái Hiên Hoàng Dương</p>
+                            </div>
+                            <p class="fg-text">Like để cập nhật những thông tin hoạt động mới nhất</p>
+                            <a href="#" class="fp-link"><span>THAM GIA NGAY</span></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sx-1">
+                        <div class="fp-wrapper">
+                            <div class="img-fanpage">
+                                <img src="./assets/images/icon-youtube.png" alt="">
+                                <p>Fanpage Mái Hiên Hoàng Dương</p>
+                            </div>
+                            <p class="fg-text">Like để cập nhật những thông tin hoạt động mới nhất</p>
+                            <a href="#" class="fp-link"><span>THAM GIA NGAY</span></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sx-1">
+                        <div class="fp-wrapper">
+                            <div class="img-fanpage">
+                                <img src="./assets/images/icon-zalo.png" alt="">
+                                <p>Fanpage Mái Hiên Hoàng Dương</p>
+                            </div>
+                            <p class="fg-text">Like để cập nhật những thông tin hoạt động mới nhất</p>
+                            <a href="#" class="fp-link"><span>THAM GIA NGAY</span></a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="main-footer">
+            <h2 class="main-footer-title">Mái Hiên Mái Xếp 247</h2>
+            <p class="copy-right">Copyright © 2022 Mái Hiên Mái Xếp Hoàng Dương. All rights reserved. Design by
+                SotaGroup Co.,Ltd</p>
+        </div>
+    </footer>
---
+++ map.php
@@ -0,0 +1,12 @@
+<div class="map">
+  <div class="container">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.6308789197965!2d106.63423761428754!3d10.839534160990924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752983c838188b%3A0xc37768d77e75a0e9!2zMTYvOUEgUGhhbiBIdXkgw41jaCwgUGjGsOG7nW5nIDE0LCBHw7IgVuG6pXAsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1645715983160!5m2!1svi!2s"
+      width="600"
+      height="450"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+    ></iframe>
+  </div>
+</div>
